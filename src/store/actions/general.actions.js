48d1bfd6f6af977cbeb99f45ee6a238ebe257992
Mohan Sagar <mohansgr3@gmail.com>
2024-09-13 08:39:02 +0530

    clean up

--- src/store/actions/general.actions.js
+++
@@ -1,9 +0,0 @@
-import { CHANGE_LOADER_STATUS } from "./types";
-
-export const startSpinner = (dispatch) => {
-  dispatch({ type: CHANGE_LOADER_STATUS, payload: true });
-};
-
-export const stopSpinner = (dispatch) => {
-  dispatch({ type: CHANGE_LOADER_STATUS, payload: false });
-};
